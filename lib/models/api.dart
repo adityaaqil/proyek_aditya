24dede3cc09291c140502d11acf2e96359640e3f
adityaaqil <151823508+adityaaqil@users.noreply.github.com>
2025-02-26 13:08:29 +0700

    Add files via upload

---
+++ lib/models/api.dart
@@ -0,0 +1,7 @@
+class BaseUrl{
+  static String tambah = "http://192.168.24.243/BIODATA/create.php";
+  static String lihat = "http://192.168.24.243/biodata/details.php";
+  static String edit = "http://192.168.24.243/biodata/update.php";
+  static String hapus = "http://192.168.24.243/biodata/delete.php";
+  static String data = "http://192.168.24.243/biodata/list.php";
+}
